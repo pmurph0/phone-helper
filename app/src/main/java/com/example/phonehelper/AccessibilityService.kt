99314e4289faa024e04f006eebae5f8cd1b5a2dc
Peter Murphy <peter.murphy@sohohouse.com>
2020-05-03 13:21:30 +0100

    init repo, add view and overlay view and gesture detector

---
+++ app/src/main/java/com/example/phonehelper/AccessibilityService.kt
@@ -0,0 +1,102 @@
+package com.example.phonehelper
+
+import android.accessibilityservice.AccessibilityService
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.Color
+import android.graphics.PixelFormat
+import android.graphics.drawable.ColorDrawable
+import android.util.Log
+import android.util.TypedValue
+import android.view.*
+import android.view.WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+import android.view.accessibility.AccessibilityEvent
+
+
+class AccessibilityService : AccessibilityService() {
+    override fun onInterrupt() {
+    }
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent) {
+    }
+
+    override fun onServiceConnected() {
+        //Add view
+        addOverlayView()
+    }
+
+    private fun addOverlayView() {
+        val view = View(this).apply {
+            background = ColorDrawable(Color.WHITE)
+        }
+        val windowManager: WindowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        windowManager.addView(view, WindowManager.LayoutParams(
+            getTouchAreaWidth(),
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY,
+            WindowManager.LayoutParams.FLAG_IGNORE_CHEEK_PRESSES
+            or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+            or FLAG_SHOW_WHEN_LOCKED,
+            PixelFormat.TRANSLUCENT
+        ).apply {
+            gravity = Gravity.RIGHT
+        })
+        val gestureDetector = GestureDetector(this, gestureListener)
+        view.setOnTouchListener { _, motionEvent ->
+            gestureDetector.onTouchEvent(motionEvent)
+            true
+        }
+    }
+
+    private fun getTouchAreaWidth(): Int {
+        val dip = 15f
+        val r: Resources = resources
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dip,
+            r.displayMetrics
+        ).toInt()
+    }
+
+    private val gestureListener = object: GestureDetector.SimpleOnGestureListener() {
+        override fun onFling(
+            e1: MotionEvent,
+            e2: MotionEvent,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            log("onFling")
+            if (e1.y > e2.y) {
+                //fling up
+                volumeUp()
+            } else {
+                //fling down
+                volumeDown()
+            }
+            return true
+        }
+
+        override fun onDoubleTapEvent(e: MotionEvent?): Boolean {
+            tryOpenNavDrawer()
+            return true
+        }
+    }
+
+    private fun log(msg: String) {
+        Log.d(com.example.phonehelper.AccessibilityService::class.java.simpleName, msg)
+    }
+
+    private fun volumeDown() {
+        log("volume down")
+        //TODO
+    }
+
+    private fun volumeUp() {
+        log("volume up")
+        //TODO
+    }
+
+    private fun tryOpenNavDrawer() {
+        log("open nav drawer")
+    }
+}
