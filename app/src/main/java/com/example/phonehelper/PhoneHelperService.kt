25681da30f73adf7bffad2e9aee763ca7e48b268
Peter Murphy <peter.murphy@sohohouse.com>
2020-05-10 10:50:35 +0100

    detect current app more accurately

--- app/src/main/java/com/example/phonehelper/PhoneHelperService.kt
+++ app/src/main/java/com/example/phonehelper/PhoneHelperService.kt
@@ -1,26 +1,37 @@
 package com.example.phonehelper
 
 import android.accessibilityservice.AccessibilityService
+import android.content.Context
+import android.hardware.display.DisplayManager
+import android.view.Display
 import android.view.accessibility.AccessibilityEvent
 import com.example.phonehelper.features.FeatureBuilder
 import com.example.phonehelper.features.edge.*
 import com.example.phonehelper.features.integrated.IntegratedFeature
 
 
-class AccessibilityService : AccessibilityService(), EdgeOverlayViewManager.Listener {
-
+class PhoneHelperService : AccessibilityService(), EdgeOverlayViewManager.Listener {
 
     private lateinit var integratedFeatures: List<IntegratedFeature>
     private lateinit var edgeGestureFeatures: Map<EdgeGestureTrigger, EdgeFeature>
     private lateinit var edgeOverlayViewManager: EdgeOverlayViewManager
+    private var currentApp  = ""
+        set(value) {
+            log("currentApp is $value")
+            field = value
+        }
 
     private val preferences: Preferences by lazy { Preferences(this) }
 
-    private var currentApp: String = ""
-        set(value) {
-            field = value
-            log("current app: $value")
+    private val isScreenOn: Boolean get() {
+        val dm = getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+        for (display in dm.displays) {
+            if (display.state != Display.STATE_OFF) {
+                return true
+            }
         }
+        return false
+    }
 
     override fun onInterrupt() {
     }
@@ -41,8 +52,16 @@ class AccessibilityService : AccessibilityService(), EdgeOverlayViewManager.List
     }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
+        log("${event.eventType.mapToEventType()} ${event.packageName}")
+
         if (event.eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
-            currentApp = event.packageName?.toString()?.takeIf { it != AppIds.SYSTEM_UI } ?: currentApp
+            when (event.packageName) {
+                AppIds.SYSTEM_UI -> {
+                    if (!isScreenOn) currentApp = AppIds.SYSTEM_UI
+                }
+                this.packageName -> {}  //do nothing for our package //TODO ?
+                else -> currentApp = event.packageName?.toString() ?: currentApp
+            }
         }
 
         integratedFeatures.forEach {
