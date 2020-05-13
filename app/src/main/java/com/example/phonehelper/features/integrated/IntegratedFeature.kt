62d1d37342687c198fd7e81846ca0f002e9d1a26
Peter Murphy <peter.murphy@sohohouse.com>
2020-05-13 22:11:03 +0100

    add TODO

--- app/src/main/java/com/example/phonehelper/features/integrated/IntegratedFeature.kt
+++ app/src/main/java/com/example/phonehelper/features/integrated/IntegratedFeature.kt
@@ -5,6 +5,7 @@ import android.view.accessibility.AccessibilityEvent
 
 //An integrated feature can listen to all AccessibilityService events and activate (including add/remove overlay UI) at any time.
 // It basically attaches to the accessibility service and manages itself.
+//TODO make this an interface?
 abstract class IntegratedFeature(protected val accessibilityService: AccessibilityService) {
 
     abstract fun onServiceConnected()
